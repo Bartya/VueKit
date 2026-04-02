e5a64b251c704692e3e4f18f0a8aae91865ba127
Your Name <liguangchao>
2026-04-02 10:28:01 +0800

    feat: 集成 unplugin-auto-import 实现模块自动导入
    - 支持 Vue/Vue Router API 自动导入
    - 自动生成类型声明文件 auto-imports.d.ts
    - 配置 include 和 exclude 规则
    - 优化开发体验，减少重复 import 代码

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+
+const message = ref("Hello World");
+</script>
 
 <template>
-  <h1>You did it!</h1>
+  <h1>{{ message }}</h1>
   <p>
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,21 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    AutoImport({
+      imports: ['vue'], // 自动导入 vue 的所有API
+      dts: 'src/auto-imports.d.ts', // 自动生成类型声明（很重要）
+    }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
